359ee4180abd67aa31023aeeb0965a3d343c18ec
Felipe Rocha <felipemota.rocha@gmail.com>
2024-04-15 18:05:56 -0300

    chore: add initial prisma schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>layout</h1>
+        {children}
+      </body>
     </html>
   );
 }
