6f63356e72f9515c5104b3dd91ed93b13cff8740
Felipe Rocha <felipemota.rocha@gmail.com>
2024-04-30 21:08:21 -0300

    feat: ensure juices from product restaurants are listed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -19,20 +19,23 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product?.restaurant.id,
       },
     },
     include: {
       restaurant: true,
     },
   });
-
-  if (!product) {
-    return notFound();
-  }
 
   return (
     <div>
