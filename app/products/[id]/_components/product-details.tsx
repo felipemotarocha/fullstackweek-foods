e239661c211853fce7d65c9ed8eedc91727da72a
Felipe Rocha <felipemota.rocha@gmail.com>
2024-04-30 21:16:31 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -146,6 +146,10 @@ const ProductDetails = ({
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   );
 };
