a8202d5e8db2c7fa0068d18d9131542905a2fadd
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-02 10:51:30 -0300

    style: add font Poppins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
